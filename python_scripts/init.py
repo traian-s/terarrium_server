d6b12996f2895eb0e937597150db83aaa259356b
Traian Stanciu <stanciu.traian@gmail.com>
2020-05-23 14:55:52 +0300

    init

---
+++ python_scripts/init.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
---
+++ python_scripts/off.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+
+GPIO.output(17,GPIO.HIGH)
+GPIO.output(27,GPIO.HIGH)
+GPIO.output(22,GPIO.HIGH)
+GPIO.output(23,GPIO.HIGH)
